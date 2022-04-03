e9fa672bde4c0765cc1c2b12cfe238e606552b9e
Ismael Chavez <chavez.ismael7@gmail.com>
2022-04-03 14:43:06 -0700

    Added sensor calibration script

---
+++ distance_calibration/distance_calibration.ino
@@ -0,0 +1,31 @@
+#define TRIG_ON PORTB |= (1<<PINB1);
+#define TRIG_OFF PORTB &= ~(1<<PINB1);
+
+int trigPin = 9;
+int echoPin = 8;
+
+long getDistance()
+{
+  long dist_m = 0;
+  long duration = 0;
+
+  TRIG_ON
+  delayMicroseconds(10);
+  TRIG_OFF
+
+  duration = pulseIn(echoPin, HIGH, 150000L);
+  dist_m = duration/58;
+  return dist_m; 
+}
+
+void setup() {
+  DDRB &= ~(1<<DDB0); //Set pin B0 as input
+  DDRB |= (1<<DDB1); //Set pin B1 as output
+  TRIG_OFF
+  Serial.begin(9600);
+}
+
+void loop() {
+  long distance = getDistance();
+  Serial.println(distance);
+}
